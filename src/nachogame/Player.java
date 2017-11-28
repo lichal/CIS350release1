8deb957600dc1b58e153be4862dd0d30c08ebf72
JON.DEWENT <dewentjo@mail.gvsu.edu>
2017-11-28 10:07:49 -0500

    Added Player Class and Rank enum
    Added a player class that has a fireRate and Rank. The Rank determines
    the fire rate. The panel will then then need to pull the fire rate from
    the player using the getFireRate method provided in player

---
+++ src/nachogame/Player.java
@@ -0,0 +1,64 @@
+package nachogame;
+
+public class Player {
+
+	private int xp;
+	private Rank rank;
+	private int fireRate;
+	
+	public Player() {
+		xp = 10;
+		getRank();
+	}
+
+	private Rank getRank() {
+		if(xp > 1000) {
+			rank = Rank.CEO;
+			fireRate = 16;
+		}
+		
+		else if(xp > 750) {
+			rank = Rank.DIRECTOR;
+			fireRate = 20;
+		}
+		
+		else if(xp > 600) {
+			rank = Rank.DISTRICT_SUPERVISOR;
+			fireRate = 25;
+		}
+		
+		else if(xp > 500) {
+			rank = Rank.FRANCHISE_OWNER;
+			fireRate = 30;
+		}
+		
+		else if(xp > 400) {
+			rank = Rank.STORE_MANAGER;
+			fireRate = 35;
+		}
+		
+		else if (xp > 300) {
+			rank = Rank.NACHO_ASSISTANT;
+			fireRate = 38;
+		}
+		
+		else if (xp > 200) {
+			rank = Rank.A_CUT_ABOVE_THE_CHEESE;
+			fireRate = 40;
+		}
+		else if (xp > 100) {
+			rank = Rank.LIMPNACHO;
+			fireRate = 45;
+		}
+		
+		else if (xp > 50) {
+			rank = Rank.CRUMBS;
+			fireRate = 50;
+		}		
+		return rank;
+	}
+	
+	public int getFireRate() {
+		return fireRate;
+	}
+}
